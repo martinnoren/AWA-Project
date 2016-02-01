cfbc52acb9a59a3d261ce243308a0c776c2a50ea
Martin Norén <martinnoren@me.com>
2016-02-01 10:44:03 +0100

    1 Februari -15 11:00

--- header.php
+++ header.php
@@ -4,8 +4,18 @@
 		<meta charset="utf-8">
 		<title>AWA-Project</title>
 		<?php wp_head(); ?>
-	</head>	
+	</head>
 
-	<body>
+	<?php 
+
+		if( is_front_page() ):
+			$awa_classes = array('awa-class', 'my-class' );
+		else:
+			$awa_classes = array( 'no-awa-class');
+		endif;
+
+	?>	
+
+	<body <?php body_class( $awa_classes ); ?>>
 
 		<?php wp_nav_menu(array('theme_location'=>'primary')); ?>
